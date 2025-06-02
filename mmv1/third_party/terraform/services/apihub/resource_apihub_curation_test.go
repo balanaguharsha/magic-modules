bf9fc5195607681d60fa4170ea516cd39e9afbd4
Balanagu Harsha Vardhan <balanaguharsha@google.com>
2025-06-02 19:18:49 +0530

    Add Curation resource of API hub

--- mmv1/third_party/terraform/services/apihub/resource_apihub_curation_test.go
+++ mmv1/third_party/terraform/services/apihub/resource_apihub_curation_test.go
@@ -9,9 +9,7 @@ import (
 )
 
 func TestAccApihubCuration_apihubCurationBasic_Update(t *testing.T) {
-	// This is added for reference, but the test needs to be skipped as it needs API hub instance as a prerequisite
-	// But the support for that resources is not yet complete.
-	t.Skip()
+	t.Parallel()
 
 	context := map[string]interface{}{
 		"random_suffix": acctest.RandString(t, 10),
@@ -23,6 +21,7 @@ func TestAccApihubCuration_apihubCurationBasic_Update(t *testing.T) {
 		ExternalProviders: map[string]resource.ExternalProvider{
 			"time": {},
 		},
+		CheckDestroy: testAccCheckApihubCurationDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccApihubCuration_apihubCuration_basic(context),
